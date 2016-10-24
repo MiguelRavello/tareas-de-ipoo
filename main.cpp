26b2e9ea46b5c9bcef7360f33636c54fa2d22978
molravinof <mol.ravinof@gmail.com>
2016-10-24 12:59:42 -0500

    la main del header

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+#include "punto.h"
+//using namespace std;
+
+int main () {
+	Poligon P1(2,2,4);
+	P1.llenar_lista();
+	P1.areaPoligono();
+	area_t s1;
+	s1=P1.getArea();
+	cout<<s1<<endl;
+	return 0;
+}
+
